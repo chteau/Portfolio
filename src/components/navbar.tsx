2eddc1ce31fef7414ce1d86797021ed341437b29
MartinPJB <29103374+MartinPJB@users.noreply.github.com>
2026-02-09 18:19:30 +0100

    Fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,4 @@
 import { Dock, DockIcon } from "@/components/magicui/dock";
-import { ModeToggle } from "@/components/mode-toggle";
 import { Separator } from "@/components/ui/separator";
 import {
   Tooltip,
